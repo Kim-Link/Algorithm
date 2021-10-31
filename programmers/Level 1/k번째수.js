532a151e5a522057bc899b24984d8c846343cb4e
Kim-Link <wildrays@gmail.com>
2021-10-31 23:30:10 +0900

    k번째 수

--- programmers/Level 1/k번째수.js
+++ programmers/Level 1/k번째수.js
@@ -1,7 +1,7 @@
 function solution(array, commands) {
     let result = [];
     for(let i = 0; i < commands.length; i++){
-        let pushItem = array.slice(commands[i][0]-1,commands[i][1]).sort()
+        let pushItem = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b) => a - b)
         result.push(pushItem[commands[i][2]-1])
     }
     return result;
